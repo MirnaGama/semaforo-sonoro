3618638eb42ba3f792e8338edd86490b2a929f3f
Mirna Gama <mirnabeatrizgamav@gmail.com>
2022-09-14 22:25:53 -0300

    Utilizando padrao de orientacao a objeto

--- main.ino
+++ main.ino
@@ -1,64 +1,81 @@
+class SemaforoPino {
+  int Luz;
+  
+  public:
+ SemaforoPino(int luz)
+ {
+  Luz = luz;
+  pinMode(Luz, OUTPUT);     
+ }
+  
+ void LIGAR()
+ {
+  digitalWrite(Luz, HIGH);
+ }   
+  
+ void DESLIGAR()
+ {
+  digitalWrite(Luz, LOW);
+ } 
+};
+
 int botao = 8;
-int carrovermelho = 13;
-int carroamarelo = 12;
-int carroverde = 11;
-int pedestrevermelho = 7;
-int pedestreverde = 6;
 int som = 10;
 int start = 0;
+SemaforoPino MotoristaVermelho(13);
+SemaforoPino MotoristaAmarelo(12);
+SemaforoPino MotoristaVerde(11);
+SemaforoPino PedestreVermelho(7);
+SemaforoPino PedestreVerde(6);
 
 void setup()
 {
   pinMode(botao, INPUT);
-  pinMode(carrovermelho, OUTPUT);
-  pinMode(carroamarelo, OUTPUT);
-  pinMode(carroverde, OUTPUT);
-  pinMode(pedestrevermelho, OUTPUT);
-  pinMode(pedestreverde, OUTPUT);
   pinMode(som, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(carroverde, LOW);
-  digitalWrite(pedestrevermelho, HIGH);
-
+  
   start = digitalRead(botao);
 
   if (start == HIGH)
   {
-    digitalWrite(carroverde, LOW);
-    digitalWrite(carroamarelo, HIGH);
+    MotoristaVerde.DESLIGAR();
+    MotoristaAmarelo.LIGAR();
     delay(1000);
-    digitalWrite(carroamarelo, LOW);
-    digitalWrite(carrovermelho, HIGH);
+    MotoristaAmarelo.DESLIGAR();
+    MotoristaVermelho.LIGAR();
     
     delay(2000);
 
     for (int i = 0; i < 10; i++)
     {
-      digitalWrite(pedestrevermelho, LOW);
-      digitalWrite(pedestreverde, HIGH);
+      MotoristaVermelho.DESLIGAR();
+      PedestreVerde.LIGAR();
       tone(som, 440, 500);
       delay(1000);
     }
 
     for (int y = 0; y < 5; y++)
     {
-      digitalWrite(pedestreverde, LOW);
+      PedestreVerde.DESLIGAR();
       tone(som, 440, 500);
-      digitalWrite(pedestrevermelho, HIGH);
+      PedestreVermelho.LIGAR();
       delay(500);
-      digitalWrite(pedestrevermelho, LOW);
+      PedestreVermelho.DESLIGAR();
       delay(500);
     }
 
-    digitalWrite(pedestrevermelho, HIGH);
+    PedestreVermelho.DESLIGAR();
     delay(3000);
 
-    digitalWrite(carrovermelho, LOW);
-    digitalWrite(carroverde, HIGH);
+    MotoristaVermelho.DESLIGAR();
+    MotoristaVerde.LIGAR();
 
     delay(15000);
+  } else {
+    MotoristaVerde.DESLIGAR();
+    MotoristaVermelho.LIGAR();
   }
 }
